633ee93f4bcd3abcbc685c337424ff6f1c556350
LluisManas <manastorra@gmail.com>
2020-05-14 11:36:40 +0200

    ready to deploy

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const User = require("./Models/User");
 const user = require("./routes/api/user");
 const movies = require("./routes/api/movies");
+const path = require("path");
 
 const app = express();
 
@@ -18,6 +19,15 @@ mongoose
 app.use("/api/user", user);
 app.use("/api/movies", movies);
 app.use("/api/auth", require("./routes/api/auth"));
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const port = 5008;
 
